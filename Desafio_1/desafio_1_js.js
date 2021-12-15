8860a036bda102f44f4e6e660066e0cec1a2d772
magalinovoa <novoamagali@gmail.com>
2021-12-14 21:18:05 -0300

    commit inicial

---
+++ Desafio_1/desafio_1_js.js
@@ -0,0 +1,9 @@
+let nombre = prompt('¿Cómo es tu nombre?');
+
+let año = parseInt(prompt('¿En qué año naciste?'));
+
+const actual = 2021
+
+let resultado = actual - año;
+
+alert ('¡Hola, ' + nombre + '!' + ' Tenes ' + resultado + ' años de edad.' );
